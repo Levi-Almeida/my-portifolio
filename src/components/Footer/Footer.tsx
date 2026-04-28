3da45051402f5740d40999aa47fcbcec6483d73d
Levi-Almeida <lev02almeida@gmail.com>
2026-04-28 15:14:48 -0300

    feat: Add WPP button to Header

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,26 +1,36 @@
 import { Box, Container, IconButton, Typography } from "@mui/material"
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
 
 const Footer: React.FC = () => {
 
+
+    const handleWhatsApp = () => {
+        const message = encodeURIComponent(
+            "Olá Levi, vi seu portfólio e gostaria de conversar!"
+        );
+
+        window.open(`https://wa.me/5511914898784?text=${message}`, "_blank");
+    };
+
     return (
         <>
-            <Box sx={{pt:2, pb:2}}>
+            <Box sx={{ pt: 2, pb: 2 }}>
                 <Container maxWidth="sm">
-                    <Box   sx={{pb:1, display:"flex", alignItems:"center", justifyContent:"space-around"}} >
+                    <Box sx={{ pb: 1, display: "flex", alignItems: "center", justifyContent: "space-around" }} >
                         <IconButton onClick={() => window.open("https://github.com/Levi-Almeida")}>
                             <GitHubIcon />
                         </IconButton>
                         <IconButton onClick={() => window.open("https://www.linkedin.com/in/Levi-Almeida/")}>
                             <LinkedInIcon />
                         </IconButton>
-                        <IconButton href="mailto:levi02almeida@gmail.com" target="_blank">
-                            <EmailIcon />
+                        <IconButton onClick={() => handleWhatsApp()}>
+                            <WhatsAppIcon />
                         </IconButton>
                     </Box>
-                    <Typography sx={{ textAlign:"center"}}>
+                    <Typography sx={{ textAlign: "center" }}>
                         © 2026 Levi Almeida - All rights reserved
                     </Typography>
                 </Container>
